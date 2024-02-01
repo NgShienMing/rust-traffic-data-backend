405be5f47f384a735c59bd28619c0837be5f9adc
Ng Shien Ming <nsming1030@gmail.com>
2024-02-01 19:15:55 +0800

    sample sql

---
+++ migrations/20240201_INSERT_DATA.sql
@@ -0,0 +1,4 @@
+INSERT INTO traffic_data (source, category, device, objects_count, vehicles_count, "timestamp") VALUES
+    ('datamall2', 'datamall', '1003', 10, 10, '2020-01-01 00:00:00'),
+    ('dbkl', 'Jalan Loke Yew' , 'A027F', 6, 6, '2023-12-01 09:50:47.787621'),
+    ('llm', 'NKV', 'PLS CAM C4 DAMANSARA KM17.95 NB', 19, 19, '2023-11-30 15:33:37.375955');
