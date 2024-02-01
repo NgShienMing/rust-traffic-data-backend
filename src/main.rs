174dc7a6494c0d3ff0cd1ce0031761a6a0904db6
Ng Shien Ming <nsming1030@gmail.com>
2024-02-01 19:15:17 +0800

    feat: main initialization script for rust backend

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,62 @@
-use axum::{
-    routing::get,
-    Router,
-    Json,
-    response::IntoResponse,
+use std::sync::Arc;
+
+use axum::http::{
+    header::{AUTHORIZATION, ACCEPT, CONTENT_TYPE},
+    HeaderValue,
+    Method,
 };
+use dotenv::dotenv;
+use sqlx::{
+    postgres::PgPoolOptions,
+    Pool,
+    Postgres
+};
+use tower_http::cors::CorsLayer;
 
 mod models;
 mod routes;
 mod controllers;
 
-async fn traffic_data_handler() -> impl IntoResponse {
-    const MESSAGE: &str = "Simple CRUD API with Rust, SQLX, Postgres,and Axum";
-    let json_response = serde_json::json!({
-        "status": "success",
-        "message": MESSAGE
-    });
-    Json(json_response)
+pub struct AppState {
+    db: Pool<Postgres>,
 }
 
 #[tokio::main]
 async fn main() {
-    dotenv::dotenv().ok();
-    // build our application with a single route
-    let app = Router::new().route("/api/trafficdata", get(traffic_data_handler));
+    dotenv().ok();
+
+    // Connect to the database
+    let database_url: String = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool: Pool<Postgres> = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("✅Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            println!("🔥 Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
+
+    // Build our application with a single route
+    let app_state: Arc<AppState> = Arc::new(AppState{db: pool.clone()});
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+        .allow_credentials(true)
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
+
+    let app = routes::create_router(app_state).layer(cors);
+
     println!("Server started successfully!");
     println!("Server running on port 3000");
-    println!("Visit http://localhost:3000/api/trafficdata to see the response");
+    println!("Visit http://localhost:3000/api/traffic to see the response");
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener: tokio::net::TcpListener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
