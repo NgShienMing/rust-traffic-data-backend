a4f5c5d06c3b3c4eea396d957c20aed972873d8f
Ng Shien Ming <nsming1030@gmail.com>
2024-02-02 10:04:23 +0800

    feat: add simple entry route

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ async fn main() {
 
     println!("Server started successfully!");
     println!("Server running on port 3000");
-    println!("Visit http://localhost:3000/api/traffic to see the response");
+    println!("Visit http://localhost:3000/api to see the response");
 
     // run our app with hyper, listening globally on port 3000
     let listener: tokio::net::TcpListener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,12 +1,33 @@
 use std::sync::Arc;
 
-use axum::Router;
+use axum::{
+    Json,
+    Router,
+    routing::get,
+    response::IntoResponse,
+};
 
 use crate::AppState;
 
 mod traffic_route;
 
+async fn entry_route_handler() -> impl IntoResponse {
+    const MESSAGE: &str = "Simple CRUD API with Rust, SQLX, Postgres and Axum";
+    let json_response = serde_json::json!({
+        "status": "success",
+        "message": MESSAGE
+    });
+    Json(json_response)
+}
+
+fn entry_route(app_state: Arc<AppState>) -> Router {
+    Router::new()
+        .route("/", get(entry_route_handler))
+        .with_state(app_state)
+}
+
 pub fn create_router(app_state: Arc<AppState>) -> Router {
     Router::new()
-    .nest("/api/traffic", traffic_route::traffic_routes(app_state))
+    .nest("/api", entry_route(app_state.clone()))
+    .nest("/api/traffic", traffic_route::traffic_routes(app_state.clone()))
 }
