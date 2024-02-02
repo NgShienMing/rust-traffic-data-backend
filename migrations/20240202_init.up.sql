b1f8838b352a3a5bce612308654ac02ee62f7d97
Ng Shien Ming <nsming1030@gmail.com>
2024-02-02 10:07:39 +0800

    feat: db table up and down

---
+++ migrations/20240202_init.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS traffic_data (
+    id SERIAL PRIMARY KEY NOT NULL,
+    source TEXT NOT NULL,
+    category TEXT NOT NULL,
+    device TEXT NOT NULL,
+    objects_count INT NOT NULL,
+    vehicles_count INT NOT NULL,
+    "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL,
+);
